1271a194b6888f0fe66694b167d4544cc8042aec
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2020-01-19 19:00:47 +0100

    Alcune correzioni e i risultati della calibrazione rc

--- pru/libs/controller/pru_controller.c
+++ pru/libs/controller/pru_controller.c
@@ -75,10 +75,10 @@ struct pru_controller_status pru_controller_status_val = {
                                                           {0, 0, 0, 0}  // M
 };
 struct pru_controller_config pru_controller_config_val = {
-                                                          0x0080,    // ke: fix point 8 bits (0x0100 corrisponde a 1)
+                                                          0x0100,    // ke: fix point 8 bits (0x0100 corrisponde a 1)
                                                           0x0000,    // ki: fix point 8 bits
                                                           0x0000,    // kd: fix point 8 bits
-                                                          0x0080,    // yke: fix point 8 bits (0x0100 corrisponde a 1)
+                                                          0x0100,    // yke: fix point 8 bits (0x0100 corrisponde a 1)
                                                           0x0000,    // yki: fix point 8 bits
                                                           0x0000,    // ykd: fix point 8 bits
                                                           0x13,      // kgyro: fix point 5 bit
@@ -118,7 +118,7 @@ void pru_controller_apply(int16_t* rc, int16_t* accel, int16_t* gyro) {
         return;
     }
     prevErr = pru_controller_status_val.MErr[POS_YAW];
-    pru_controller_status_val.MErr[POS_YAW] = rc[POS_YAW] - gyro[POS_YAW];
+    pru_controller_status_val.MErr[POS_YAW] = LIMIT(rc[POS_YAW] - gyro[POS_YAW], 10000, -10000);
     pru_controller_status_val.MDErr[POS_YAW] = pru_controller_status_val.MErr[POS_YAW] - prevErr;
     pru_controller_status_val.MIErr[POS_YAW] += pru_controller_status_val.MErr[POS_YAW]; // <---TODO: Gestire limiti max, min
     pru_controller_status_val.M[POS_YAW] = ((pru_controller_config_val.yke*pru_controller_status_val.MErr[POS_YAW]) >> 8)+
@@ -126,7 +126,7 @@ void pru_controller_apply(int16_t* rc, int16_t* accel, int16_t* gyro) {
                                            ((pru_controller_config_val.ykd*pru_controller_status_val.MDErr[POS_YAW]) >> 8);
 
     prevErr = pru_controller_status_val.MErr[POS_PITCH];
-    pru_controller_status_val.MErr[POS_PITCH] = rc[POS_PITCH] - gyro[POS_PITCH];
+    pru_controller_status_val.MErr[POS_PITCH] = LIMIT(rc[POS_PITCH] - gyro[POS_PITCH], 10000, -10000);
     pru_controller_status_val.MDErr[POS_PITCH] = pru_controller_status_val.MErr[POS_PITCH] - prevErr;
     pru_controller_status_val.MIErr[POS_PITCH] += pru_controller_status_val.MErr[POS_PITCH]; // <---TODO: Gestire limiti max, min
     pru_controller_status_val.M[POS_PITCH] = ((pru_controller_config_val.ke*pru_controller_status_val.MErr[POS_PITCH]) >> 8)+
@@ -134,7 +134,7 @@ void pru_controller_apply(int16_t* rc, int16_t* accel, int16_t* gyro) {
                                              ((pru_controller_config_val.kd*pru_controller_status_val.MDErr[POS_PITCH]) >> 8);
 
     prevErr = pru_controller_status_val.MErr[POS_ROLL];
-    pru_controller_status_val.MErr[POS_ROLL] = rc[POS_ROLL] - gyro[POS_ROLL];
+    pru_controller_status_val.MErr[POS_ROLL] = LIMIT(rc[POS_ROLL] - gyro[POS_ROLL], 10000, -10000);
     pru_controller_status_val.MDErr[POS_ROLL] = pru_controller_status_val.MErr[POS_ROLL] - prevErr;
     pru_controller_status_val.MIErr[POS_ROLL] += pru_controller_status_val.MErr[POS_ROLL]; // <---TODO: Gestire limiti max, min
     pru_controller_status_val.M[POS_ROLL] = ((pru_controller_config_val.ke*pru_controller_status_val.MErr[POS_ROLL]) >> 8)+
